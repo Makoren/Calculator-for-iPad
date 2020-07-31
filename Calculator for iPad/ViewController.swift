e357ca3215ee19ebc97a340942c052484a3591d4
Makoren <makorenwastaken@gmail.com>
2020-07-31 10:36:35 +1000

    Initial Commit

---
+++ Calculator for iPad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator for iPad
+//
+//  Created by Luke Lazzaro on 31/7/20.
+//  Copyright © 2020 Luke Lazzaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
